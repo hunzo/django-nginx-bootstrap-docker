910056386adbb5c56e33d7015c5564d540775ac2
hunzo <takraw_s@hotmail.com>
2022-05-10 13:23:43 +0700

    update x-forward-for

--- app/views.py
+++ app/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    x_forwarded_for = request.META.get('X-Forwarded-For')
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
     context = {
+        "META": request.META,
         "ip": ip
     }
     return render(request, 'contents.html', context)
