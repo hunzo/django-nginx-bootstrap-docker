09f42be13304897fe29c4f7eef31e0a380f3a78e
hunzo <takraw_s@hotmail.com>
2022-05-11 09:19:21 +0700

    add header nginx

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    x_forwarded_for = request.META.get('HTTP_X_REAL_IP')
 
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
@@ -13,4 +13,5 @@ def index(request):
         "META": request.META,
         "ip": ip
     }
+
     return render(request, 'contents.html', context)
